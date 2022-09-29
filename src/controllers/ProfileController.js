988a0109acb932fa4635f4b80c8e2a594d5ee5b3
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-30 04:12:48 +0600

    update profile done

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -28,7 +28,7 @@ exports.UserLogin = (req, res) => {
                     exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), data: data[0]
                 };
                 let token = jwt.sign(Payload, 'SecretKey12345');
-                res.status(200).json({status: "Success", token:token, data: data})
+                res.status(200).json({status: "Success", token: token, data: data})
             } else {
                 res.status(401).json({status: "Unauthorized!"})
             }
@@ -36,22 +36,33 @@ exports.UserLogin = (req, res) => {
     })
 }
 
+// Select Specific User Profile
 
-exports.SelectProfile=(req,res)=>{
-    let UserName=req.headers['username']
-    ProfileModel.find({UserName:UserName},(err,data)=>{
-        if(err){
-            res.status(401).json({status:"Failed",data:err})
-        }
-        else{
-            res.status(200).json({status:"Success",data:data})
+exports.SelectProfile = (req, res) => {
+    let UserName = req.headers['username']
+    ProfileModel.find({UserName: UserName}, (err, data) => {
+        if (err) {
+            res.status(401).json({status: "Failed", data: err})
+        } else {
+            res.status(200).json({status: "Success", data: data})
         }
     })
 }
 
-
-exports.UpdateProfile=(req,res)=>{
-    let UserName=req.headers['username']
-    let reqBody=req.body;
-    res.status(200).json(reqBody)
+// Update Profile
+//Upsert means command to replace the old data
+exports.UpdateProfile = (req, res) => {
+    let UserName = req.headers['username']
+    let reqBody = req.body;
+    ProfileModel.updateOne(
+        {UserName:UserName},
+        {$set:reqBody},
+        {upsert:true},(err,data)=>{
+            if(err){
+                res.status(400).json({status:"Failed",data:err});
+            }
+            else{
+                res.status(200).json({status:"Successfully Update the User Profile",data:data})
+            }
+        })
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -7,6 +7,7 @@ const router=express.Router();
 
 router.post("/create-profile",ProfileController.CreateProfile)
 router.post("/user-Login",ProfileController.UserLogin)
+// Token Needed
 router.get("/select-profile",AuthVerifyMiddleware,ProfileController.SelectProfile)
 router.post("/update-profile",AuthVerifyMiddleware,ProfileController.UpdateProfile)
 module.exports=router;
